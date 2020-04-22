5e3ab9ed570c385b1bb7815a390e06eccf392206
annick17 <annick2525@orange.fr>
2020-04-22 23:05:29 +0200

    modif authentification

--- ccp2/base/views.py
+++ ccp2/base/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def base(request) :
     return render (request, 'base/base.html', {})
+
 
 def accueil(request) :
     return render (request,'base/accueil.html', {})
